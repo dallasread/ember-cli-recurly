d6b15844b2c6e036d72535a04a394d3a2acbb1fe
Dallas Read <dallasread@users.noreply.github.com>
2018-06-13 15:15:32 -0300

    Allow delay of loading.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 /* jshint node: true */
 'use strict';
 
+import config from '../config/environment';
+
 module.exports = {
   name: 'recurly-service',
   contentFor: function(type) {
     // we use body since Recurly must be available before
-    if (type === 'body') {
+    if (type === 'body' && !config.recurly.delayLoading) {
       return '<script type="text/javascript" src="https://js.recurly.com/v3/recurly.js"></script>';
     }
   }
