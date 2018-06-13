45247382dafd0c1b632b9b0050d21a39d1c36b15
Dallas Read <dallasread@users.noreply.github.com>
2018-06-13 15:39:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ module.exports = {
   name: 'recurly-service',
   contentFor: function(type) {
     // we use body since Recurly must be available before
-    if (type === 'body' && !config.recurly.delayLoading) {
-      return '<script type="text/javascript" src="https://js.recurly.com/v3/recurly.js"></script>';
-    }
+    // if (type === 'body' && !config.recurly.delayLoading) {
+    //   return '<script type="text/javascript" src="https://js.recurly.com/v3/recurly.js"></script>';
+    // }
   }
 };
